52195bc40bb4f40e2376e95dfbfd15c6f98f7f43
Jacob Mischka <jacob@mischka.me>
2017-09-08 14:37:42 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+export function camelCaseToWords(str) {
+	let result = '';
+	for(let char of str) {
+		if (result === '') {
+			result += char.toUpperCase();
+		}
+		else if (char === char.toUpperCase()) {
+			result += ' ' + char.toLowerCase();
+		}
+		else {
+			result += char;
+		}
+	}
+	return result;
+}
+
+export function snakeCaseToWords(str) {
+	return str.charAt(0).toUpperCase() + str.substring(1).replace('_', ' ');
+}
+
+export function kebabCaseToWords(str) {
+	return str.charAt(0).toUpperCase() + str.substring(1).replace('-', ' ');
+}
